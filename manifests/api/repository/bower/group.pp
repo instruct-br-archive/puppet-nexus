29eda6f0bae5c6b42d8a4a4b7d3ee73fa3f2f3c1
Igor Oliveira <igor@instruct.com.br>
2018-06-25 19:05:42 -0300

    Adds Bower creation defined types and templates

--- manifests/api/repository/bower/group.pp
+++ manifests/api/repository/bower/group.pp
@@ -1,27 +1,34 @@
 # == Define: define_name
 #
 define nexus::api::repository::bower::group (
-  String $name                                         = '',
-  Array[String] $members                               = '',
-  String $blobstore_name                               = 'Default',
-  String $host                                         = $nexus::listen_address,
-  String $port                                         = $nexus::http_port,
-  String $user                                         = 'admin',
-  String $password                                     = 'admin123',
+  Array[String] $members = '',
+  String $blobstore_name = 'default',
+  String $host           = $nexus::listen_address,
+  Integer $port          = $nexus::http_port,
+  String $user           = 'admin',
+  String $password       = 'admin123',
 ) {
 
-  file { '/tmp/${name}.json':
-
+  file { "/tmp/repository-bower-${name}.json":
+    ensure  => present,
+    owner   => $nexus::user,
+    group   => $nexus::group,
+    content => epp('nexus/api/repository/bower/group.json.epp', {
+      repository_name => $name,
+      blobstore_name  => $blobstore_name,
+      members         => $members,
+    }),
   }
 
-  nexus::api::script::add {
-    path        => "/tmp/${name}.json",
-    script_name => $name,
-    host        => $host,
-    port        => $port,
-    user        => $user,
-    password    => $password,
-    run         => true,
+  nexus::api::script::add { "add-repository-bower-${name}-script":
+    path             => "/tmp/repository-bower-${name}.json",
+    script_name      => $name,
+    host             => $host,
+    port             => $port,
+    user             => $user,
+    password         => $password,
+    run              => true,
+    delete_after_run => true,
   }
 
 }
--- manifests/api/repository/bower/proxy.pp
+++ manifests/api/repository/bower/proxy.pp
@@ -1,29 +1,38 @@
 # == Define: define_name
 #
 define nexus::api::repository::bower::proxy (
-  String $name           = '',
-  String $remote_url     = 'http://www.nuget.org/api/v2/',
-  String $blobstore_name = 'Default',
+  String $remote_url     = 'https://registry.bower.io',
+  String $blobstore_name = 'default',
   Boolean $rewrite_package_urls = true,
   Boolean $strict        = true,
   String $host           = $nexus::listen_address,
-  String $port           = $nexus::http_port,
+  Integer $port          = $nexus::http_port,
   String $user           = 'admin',
   String $password       = 'admin123',
 ) {
 
-  file { '/tmp/${name}.json':
-
+  file { "/tmp/repository-bower-${name}.json":
+    ensure  => present,
+    owner   => $nexus::user,
+    group   => $nexus::group,
+    content => epp('nexus/api/repository/bower/proxy.json.epp', {
+      repository_name      => $name,
+      remote_url           => $remote_url,
+      blobstore_name       => $blobstore_name,
+      strict               => $strict,
+      rewrite_package_urls => $rewrite_package_urls,
+    }),
   }
 
-  nexus::api::script::add {
-    path        => "/tmp/${name}.json",
-    script_name => $name,
-    host        => $host,
-    port        => $port,
-    user        => $user,
-    password    => $password,
-    run         => true,
+  nexus::api::script::add { "add-repository-bower-${name}-script":
+    path             => "/tmp/repository-bower-${name}.json",
+    script_name      => $name,
+    host             => $host,
+    port             => $port,
+    user             => $user,
+    password         => $password,
+    run              => true,
+    delete_after_run => true,
   }
 
 }
