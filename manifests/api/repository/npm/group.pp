e240bf0c0453ded55c0d8909c6c478218ead809b
Igor Oliveira <igor@instruct.com.br>
2018-06-25 18:31:24 -0300

    Add Npm creation defined types

--- manifests/api/repository/npm/group.pp
+++ manifests/api/repository/npm/group.pp
@@ -1,27 +1,34 @@
 # == Define: define_name
 #
 define nexus::api::repository::npm::group (
-  String $name                                         = '',
-  Array[String] $members                               = '',
-  String $blobstore_name                               = 'Default',
-  String $host                                         = $nexus::listen_address,
-  String $port                                         = $nexus::http_port,
-  String $user                                         = 'admin',
-  String $password                                     = 'admin123',
+  Array[String] $members = '',
+  String $blobstore_name = 'default',
+  String $host           = $nexus::listen_address,
+  Integer $port          = $nexus::http_port,
+  String $user           = 'admin',
+  String $password       = 'admin123',
 ) {
 
-  file { '/tmp/${name}.json':
-
+  file { "/tmp/repository-npm-${name}.json":
+    ensure  => present,
+    owner   => $nexus::user,
+    group   => $nexus::group,
+    content => epp('nexus/api/repository/npm/group.json.epp', {
+      repository_name => $name,
+      blobstore_name  => $blobstore_name,
+      members         => $members,
+    }),
   }
 
-  nexus::api::script::add {
-    path        => "/tmp/${name}.json",
-    script_name => $name,
-    host        => $host,
-    port        => $port,
-    user        => $user,
-    password    => $password,
-    run         => true,
+  nexus::api::script::add { "add-repository-npm-${name}-script":
+    path             => "/tmp/repository-npm-${name}.json",
+    script_name      => $name,
+    host             => $host,
+    port             => $port,
+    user             => $user,
+    password         => $password,
+    run              => true,
+    delete_after_run => true,
   }
 
 }
--- manifests/api/repository/npm/proxy.pp
+++ manifests/api/repository/npm/proxy.pp
@@ -1,28 +1,36 @@
 # == Define: define_name
 #
 define nexus::api::repository::npm::proxy (
-  String $name           = '',
   String $remote_url     = 'https://registry.npmjs.org',
-  String $blobstore_name = 'Default',
+  String $blobstore_name = 'default',
   Boolean $strict        = true,
   String $host           = $nexus::listen_address,
-  String $port           = $nexus::http_port,
+  Integer $port          = $nexus::http_port,
   String $user           = 'admin',
   String $password       = 'admin123',
 ) {
 
-  file { '/tmp/${name}.json':
-
+  file { "/tmp/repository-npm-${name}.json":
+    ensure  => present,
+    owner   => $nexus::user,
+    group   => $nexus::group,
+    content => epp('nexus/api/repository/npm/proxy.json.epp', {
+      repository_name => $name,
+      remote_url      => $remote_url,
+      blobstore_name  => $blobstore_name,
+      strict          => $strict,
+    }),
   }
 
-  nexus::api::script::add {
-    path        => "/tmp/${name}.json",
-    script_name => $name,
-    host        => $host,
-    port        => $port,
-    user        => $user,
-    password    => $password,
-    run         => true,
+  nexus::api::script::add { "add-repository-npm-${name}-script":
+    path             => "/tmp/repository-npm-${name}.json",
+    script_name      => $name,
+    host             => $host,
+    port             => $port,
+    user             => $user,
+    password         => $password,
+    run              => true,
+    delete_after_run => true,
   }
 
 }
