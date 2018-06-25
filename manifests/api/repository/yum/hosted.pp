e5626c596a1d6f2ffa47cd573cdb7fdef455d243
Igor Oliveira <igor@instruct.com.br>
2018-06-25 13:44:33 -0300

    Add YumRepo creation support

--- manifests/api/repository/yum/hosted.pp
+++ manifests/api/repository/yum/hosted.pp
@@ -1,29 +1,38 @@
 # == Define: define_name
 #
 define nexus::api::repository::yum::hosted (
-  String $name           = '',
-  String $blobstore_name = 'Default',
-  Boolen $strict         = true,
-  Integer $depth         = 0,
-  Enum['allow', 'deny', 'readonly'] $write_policy      = 'allow',
+  String $blobstore_name = 'default',
+  Boolean $strict        = true,
+  Integer[0, 5] $depth   = 0,
+  Enum['ALLOW', 'ALLOW_ONCE', 'DENY'] $write_policy = 'ALLOW',
   String $host           = $nexus::listen_address,
-  String $port           = $nexus::http_port,
+  Integer $port          = $nexus::http_port,
   String $user           = 'admin',
   String $password       = 'admin123',
 ) {
 
-  file { '/tmp/${name}.json':
-
+  file { "/tmp/repository-yum-${name}.json":
+    ensure  => present,
+    owner   => $nexus::user,
+    group   => $nexus::group,
+    content => epp('nexus/api/repository/yum/hosted.json.epp', {
+      repository_name => $name,
+      blobstore_name  => $blobstore_name,
+      strict          => $strict,
+      write_policy    => $write_policy,
+      depth           => $depth,
+    }),
   }
 
-  nexus::api::script::add {
-    path        => "/tmp/${name}.json",
-    script_name => $name,
-    host        => $host,
-    port        => $port,
-    user        => $user,
-    password    => $password,
-    run         => true,
+  nexus::api::script::add { "add-repository-yum-${name}-script":
+    path             => "/tmp/repository-yum-${name}.json",
+    script_name      => $name,
+    host             => $host,
+    port             => $port,
+    user             => $user,
+    password         => $password,
+    run              => true,
+    delete_after_run => true,
   }
 
 }
